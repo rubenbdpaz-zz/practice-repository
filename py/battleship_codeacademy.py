2c1387018e2dffbc41d9623d1b33a6cec608d068
Rúben Barbosa da Paz <rubenbdpaz@gmail.com>
2016-11-28 11:22:52 -0300

    Adding () to print statements

--- py/battleship_codeacademy.py
+++ py/battleship_codeacademy.py
@@ -9,9 +9,9 @@ for x in range(5):
 
 def print_board(board):
     for row in board:
-        print " ".join(row)
+        print (" ".join(row))
 
-print "Let's play Battleship!"
+print ("Let's play Battleship!")
 print_board(board)
 
 def random_row(board):
@@ -26,27 +26,27 @@ ship_col = random_col(board)
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
 for turn in range(4):
-    print "Turn ", turn+1
+    print ("Turn ", turn+1)
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
-    
+
     if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
+        print ("Congratulations! You sunk my battleship!")
         break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
+            print ("Oops, that's not even in the ocean.")
         elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+            print ("You guessed that one already.")
             turn -= 1
         else:
-            print "You missed my battleship!"
+            print ("You missed my battleship!")
             board[guess_row][guess_col] = "X"
             if turn == 3:
-                print "Game Over"
+                print ("Game Over")
         # Print (turn + 1) here!
         print_board(board)
 
 board[ship_row][ship_col] = "B"
-print "="*10
+print ("="*10)
 print_board(board)
